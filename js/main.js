f299bed089998aa595df3c6d9e8a58ea8a38b485
webitall <welcome4899@gmail.com>
2020-02-21 12:45:51 +0200

    all fixes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var arrow_down = document.querySelector('.mot-history__box-info-arrow');
+  var arrow_down = document.querySelectorAll('.mot-history__box-info-arrow');
   var history_box = document.querySelectorAll('.mot-history__box-info');
   const history_box_more_info = document.querySelectorAll('.history__box-all-info');
+
   for(let i = 0; i < history_box.length; i++){
     history_box[i].addEventListener('click', function(){
       this.childNodes[2].nextElementSibling.classList.toggle('hide');
       if(this.childNodes[2].nextElementSibling.classList.contains('hide')){
-        arrow_down.setAttribute('src','images/arrow-down.svg');
+        arrow_down[i].setAttribute('src','images/arrow-down.svg');
       }
       else{
-        arrow_down.setAttribute('src','images/ic-arrow-up.svg');
+        arrow_down[i].setAttribute('src','images/ic-arrow-up.svg');
       }
     });
   }
